47135a05ef62c43d81c43541f616568108f641f2
tanmen <yt.prog@gmail.com>
2020-11-22 02:43:37 +0900

    Changed so that it can be generated without the index file.

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # export-indexer
 
-Testing now
+This library is a library that automatically generates an index file.
+
+## Install
+`yarn`
+```
+yarn add -D export-indexer
+```
+`npm`
+```
+npm install -D export-indexer
+```
+
+## Usage
+Create a `.index` file where you want the index file to be generated.
+
+### Example
+If the directory structure is as follows
+```
+└ components
+  ├ .index
+  └　test.tsx
+```
+
+Do the following in terminal:
+```
+yarn export-indexer
+```
+
+Then, the following contents will be output.
+`index.ts`
+```
+export * from './text';
+```
+
+`file`
+```
+└ components
+  ├ .index
+  ├ index.ts
+  └　test.tsx
+```
+
+
--- src/createIndex.ts
+++ src/createIndex.ts
@@ -1,10 +1,19 @@
 import {writeFile} from "fs/promises";
 import {join, basename, extname} from "path";
 
+const findIndexPath = (paths: string[]) => {
+  const indexPath = paths.find(file => file.startsWith('index.'));
+  if (!indexPath) {
+    const path = paths[0]
+    return path ? `index${extname(path) || '.ts'}` : undefined;
+  }
+  return indexPath;
+}
+
 export const createIndex = (dir: string, paths: string[]) => {
-  const path = paths.find(file => file.startsWith('index.'));
-  if (!path) throw new Error('Not found index file. Please create index file.');
-  const exportFiles = paths.filter(file => !file.startsWith('index.'))
+  const path = findIndexPath(paths);
+  if (!path) return Promise.resolve();
+  const exportFiles = paths.filter(file => !file.startsWith('index.'));
   return writeFile(join(dir, path), exportFiles.map(file => `export * from './${basename(file, extname(file))}';`).join('\n'),
     {encoding: 'utf-8'})
 }
