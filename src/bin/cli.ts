73aa84eb26246b18a2a89c5886c5254bfa0ca24c
tanmen <yt.prog@gmail.com>
2022-07-16 21:20:39 +0900

    remove choredir

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,25 +1,7 @@
 #!/usr/bin/env node
-import * as fs from "fs";
-import chokidar from "chokidar";
 import {indexer} from "../index";
-import {basename} from 'path';
 
-const watch = process.argv.includes('--watch');
 const extArg = process.argv.find(arg => arg.startsWith('--ext'));
 const ext = ((extArg && extArg.match(/--ext=(.+)/)?.[1]) ?? 'ts') as 'js' | 'ts';
 
-const exec = () => indexer(process.cwd(), {watch, ext});
-
 indexer(process.cwd(), {watch: false, ext});
-
-chokidar
-  .watch(process.cwd(), {
-    persistent: true,
-    ignored: '**/node_modules/**/*',
-    awaitWriteFinish: true,
-    followSymlinks: true,
-    atomic: true,
-  })
-  .on('add', () => exec())
-  .on('change', path => basename(path) === '.index' && exec())
-  .on('unlink', () => exec());
