b4eb6b2d4ddc851af12ab493c464397a5cdab263
tanmen <yt.prog@gmail.com>
2024-08-17 09:03:26 +0900

    fix(createIndex): add newline

--- src/createIndex.ts
+++ src/createIndex.ts
@@ -5,7 +5,7 @@ export const createIndex = (dir: string, paths: string[], {ext}: Option) => {
   const path = paths.find(file => file.startsWith('index.')) ?? `index.${ext}`;
   if (!path) return Promise.resolve();
   const exportFiles = paths.filter(file => !file.startsWith('index.'));
-  return writeFile(join(dir, path), exportFiles.map(file => `export * from './${basename(file, extname(file))}';`).join('\n'),
+  return writeFile(join(dir, path), exportFiles.map(file => `export * from './${basename(file, extname(file))}';\n`).join(''),
     {encoding: 'utf-8'})
 }
 
